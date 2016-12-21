ea546d94237d065798a2d7a593783231d3b65f34
NCG <NCG@DESKTOP-T6ENAEQ>
2016-12-21 14:13:07 +0900

    Student 클래스 생성

---
+++ src/kr/co/dgit/Student.java
@@ -0,0 +1,64 @@
+package kr.co.dgit;
+
+import com.mysql.fabric.xmlrpc.base.Data;
+
+public class Student {
+	private int studId;
+	private String name;
+	private String email;
+	private Data dob;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Student(int studId, String name, String email, Data dob) {
+		super();
+		this.studId = studId;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+	}
+
+
+	public int getStudId() {
+		return studId;
+	}
+
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Data getDob() {
+		return dob;
+	}
+
+	public void setDob(Data dob) {
+		this.dob = dob;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [studId=" + studId + ", name=" + name + ", email=" + email + ", dob=" + dob + "]";
+	}
+	
+	
+}
