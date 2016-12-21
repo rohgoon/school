e68e49f48b6379051d036b3387a526b4a6b39092
NCG <NCG@DESKTOP-T6ENAEQ>
2016-12-21 14:20:42 +0900

    Util 클래스 생성

---
+++ src/kr/co/dgit/util/Util.java
@@ -0,0 +1,55 @@
+package kr.co.dgit.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Util {
+	public static void close(Connection con){
+		if(con != null){
+			try {
+				con.close();
+				con = null;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	public static void close(Statement pstmt){
+		if(pstmt != null){
+			try {
+				pstmt.close();
+				pstmt = null;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public static void close(ResultSet rs){
+		if(rs != null){
+			try {
+				rs.close();
+				rs = null;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public static void close(ResultSet rs, Statement pstmt){
+		close(rs);
+		close(pstmt);	
+		
+	}
+}
